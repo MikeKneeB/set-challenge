cda1c6dac41a98678bc4a4b1ae27b683a7571387
Mike Knee <mikeknee@hotmail.co.uk>
2018-05-26 17:06:26 +0100

    ~~~~

---
+++ api/config_motors.py
@@ -0,0 +1,8 @@
+'''
+Motor configuration file - there is a small difference in power between the two
+motors, and so this file can be used to negate that difference by setting a
+small limit on either of the motors.
+'''
+
+LEFT_MOTOR_OFFSET = 0.
+RIGHT_MOTOR_OFFSET = 0.
---
+++ mover/speed_test.py
@@ -0,0 +1,26 @@
+from api import (
+    ServoController,
+    UltrasonicSensor,
+    MotorController,
+    SpeedSettings
+)
+
+from time import sleep
+
+if __name__ == '__main__':
+    servo = ServoController.ServoController()
+    motor = MotorController.MotorController(GPIOLayout.MOTOR_LEFT_FORWARD_GPIO,
+                                            GPIOLayout.MOTOR_LEFT_BACKWARD_GPIO,
+                                            GPIOLayout.MOTOR_RIGHT_FORWARD_GPIO,
+                                            GPIOLayout.MOTOR_RIGHT_BACKWARD_GPIO)
+    ultra = UltrasonicSensor.UltrasonicSensor(GPIOLayout.SONAR_FRONT_TX_GPIO,
+                                              qsize=10)
+    servo.start_servos()
+    servo.set_tilt_servo(0)
+    servo.set_pan_servo(0)
+    initial_distance = ultra.measurement()
+    motor.forward(SpeedSettings.SPEED_MEDIUM)
+    sleep(5)
+    motor.stop()
+    final_distance = ultra.measurement()
+    print(initial_distance - final_distance)
